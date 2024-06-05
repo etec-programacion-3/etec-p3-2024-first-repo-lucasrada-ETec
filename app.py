ecb859ffa7b61f01867e5dd19a943732fcd6306e
lucasrada-ETec <l.rada@alumno.etec.um.edu.ar>
2024-06-05 12:36:12 -0300

    nombreCompleto

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def hola():
 def saludo(name):
     return 'Hola ' + name
 
+@app.route('/nombreCompleto', defaults={'name': None, 'lastname': None})
+@app.route('/nombreCompleto/<name>/<lastname>')
+def nombreCompleto(name, lastname):
+    return f'Hola {name} {lastname}'
+
 @app.route('/chau/')
 def chau():
     return 'Chau'
